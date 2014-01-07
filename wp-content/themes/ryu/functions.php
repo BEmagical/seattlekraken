e769ad4477ce0c4e66d076efaa404777ba340365
Brian Enriquez <benriquez@deloitte.com>
2014-01-07 10:22:11 -0800

    move ryu theme js into assets/js/ryu directory

--- wp-content/themes/ryu/functions.php
+++ wp-content/themes/ryu/functions.php
@@ -162,17 +162,17 @@ function ryu_scripts() {
 	wp_enqueue_style( 'ryu-playfair-display' );
 
 	if ( has_nav_menu( 'primary' ) )
-		wp_enqueue_script( 'ryu-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+		wp_enqueue_script( 'ryu-navigation', get_template_directory_uri() . '/assets/js/ryu/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'ryu-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'ryu-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/ryu/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) )
 		wp_enqueue_script( 'comment-reply' );
 
 	if ( is_singular() && wp_attachment_is_image() )
-		wp_enqueue_script( 'ryu-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
+		wp_enqueue_script( 'ryu-keyboard-image-navigation', get_template_directory_uri() . '/assets/js/ryu/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 
-	wp_enqueue_script( 'ryu-theme', get_template_directory_uri() . '/js/ryu.js', array( 'jquery' ), '20130319', true );
+	wp_enqueue_script( 'ryu-theme', get_template_directory_uri() . '/assets/js/ryu/ryu.js', array( 'jquery' ), '20130319', true );
 }
 add_action( 'wp_enqueue_scripts', 'ryu_scripts' );
 
