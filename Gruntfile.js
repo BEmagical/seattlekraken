99a04b865a296c1b1ba5e63efa4d86bfe88389b7
Brian Enriquez <benriquez@deloitte.com>
2014-01-15 15:18:56 -0800

    added compass to the project for compiling the sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,28 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
 
+    compass: {
+      build: {
+        options: {
+          specify: ["wp-content/themes/ryu/assets/scss/style.scss"],
+          outputStyle: "compressed",
+          noLineComments: false,
+          sassDir: "wp-content/themes/ryu/assets/scss",
+          cssDir: "wp-content/themes/ryu",     
+        }
+      }
+    },
+
+    watch: {
+      sass: {
+        files: ["wp-content/themes/ryu/assets/scss/style.scss"],
+        tasks: ["compass:build"],
+        options: {
+          livereload: true,
+        },
+      }
+    },
+
     rsync: {
       options: {
         args: ["--verbose"],
@@ -19,9 +41,15 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.registerTask("default", "Build tasks for production.", function() {
+    grunt.task.run("compass:build", "watch");
+  });
+
   grunt.registerTask("production", "Deploy to production environment.", function() {
     grunt.task.run("rsync:production");
   });
 
   grunt.loadNpmTasks('grunt-rsync');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 }
